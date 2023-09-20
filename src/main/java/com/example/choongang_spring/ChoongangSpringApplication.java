095b202f9d3ea8cdb456bd6d58ad70dd814e4f23
soowan95 <ksw9565@gmail.com>
2023-09-20 16:20:16 +0900

    first commit

---
+++ src/main/java/com/example/choongang_spring/ChoongangSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.choongang_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoongangSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChoongangSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/choongang_spring/ChoongangSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.choongang_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoongangSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
