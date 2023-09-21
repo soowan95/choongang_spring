35b914264d026c5535553202c6bce0c0c56a27b8
soowan95 <ksw9565@gmail.com>
2023-09-21 15:34:10 +0900

    learn jsp

---
+++ src/main/java/com/example/choongang_spring/controller/Controller07.java
@@ -0,0 +1,17 @@
+package com.example.choongang_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main7")
+public class Controller07 {
+
+  // (RequestMapping) Handler Method
+  @RequestMapping("sub1")
+  public String method1() {
+    System.out.println("Controller07.method1");
+
+    return "view1";
+  }
+}
