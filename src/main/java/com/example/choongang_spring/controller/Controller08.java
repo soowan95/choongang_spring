bfbd6b395ef480f51e1591a2897b1bb339c99626
soowan95 <ksw9565@gmail.com>
2023-09-21 16:33:54 +0900

    learn model

---
+++ src/main/java/com/example/choongang_spring/controller/Controller08.java
@@ -0,0 +1,16 @@
+package com.example.choongang_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main8")
+public class Controller08 {
+
+  @RequestMapping("sub1")
+  public void method1(Model model) {
+    model.addAttribute("attr1", "value1");
+    model.addAttribute("attr2", "value2");
+  }
+}
