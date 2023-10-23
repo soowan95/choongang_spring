b6a7a74436cdd5283d267ebfcac2d6c998c5609e
soowan95 <ksw9565@gmail.com>
2023-10-23 10:11:21 +0900

    use aws s3

---
+++ src/main/java/com/example/choongang_spring/controller/Controller34.java
@@ -0,0 +1,27 @@
+package com.example.choongang_spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Controller
+@RequestMapping("main34")
+@RequiredArgsConstructor
+public class Controller34 {
+
+  private final S3Client s3Client;
+
+  @GetMapping("sub1")
+  public void method1() {
+
+  }
+
+  @PostMapping("sub2")
+  public void method2(MultipartFile file) {
+//    s3Client.putObject();
+  }
+}
